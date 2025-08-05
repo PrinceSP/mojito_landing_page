1282f460271d4c413cf7dc7c1d1ebde4e82bb383
PrinceSP <princepasombaran@gmail.com>
2025-08-05 16:24:42 +0700

    feat: create about section components + its faded animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { ScrollTrigger,SplitText } from 'gsap/all'
+import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import Navbar from './components/navbar'
 import Hero from './components/hero'
 import Cocktails from './components/cocktails'
+import About from './components/about'
 
-gsap.registerPlugin(ScrollTrigger,SplitText)
+gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
     <main>
-      <Navbar/>
-      <Hero/>
-      <Cocktails/>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About/>
     </main>
   )
 }
