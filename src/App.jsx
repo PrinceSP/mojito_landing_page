47b13da59fa05261b20063410357770a483e4986
PrinceSP <princepasombaran@gmail.com>
2025-08-05 12:19:23 +0700

    feat: adding popular and loved items sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import { ScrollTrigger,SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import Navbar from './components/navbar'
 import Hero from './components/hero'
+import Cocktails from './components/cocktails'
 
 gsap.registerPlugin(ScrollTrigger,SplitText)
 
@@ -11,6 +11,7 @@ const App = () => {
     <main>
       <Navbar/>
       <Hero/>
+      <Cocktails/>
     </main>
   )
 }
